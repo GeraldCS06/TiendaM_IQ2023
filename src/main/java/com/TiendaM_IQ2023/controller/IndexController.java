db18a1cb9f86075ca115a19e16da56272ed7fefe
Gerald0609 <66327871+Gerald0609@users.noreply.github.com>
2023-02-15 21:04:24 -0600

    Finalizacion clase 5

--- src/main/java/com/TiendaM_IQ2023/controller/IndexController.java
+++ src/main/java/com/TiendaM_IQ2023/controller/IndexController.java
@@ -1,38 +1,56 @@
 package com.TiendaM_IQ2023.controller;
 
-import com.TiendaM_IQ2023.dao.ClienteDao;
 import com.TiendaM_IQ2023.domain.Cliente;
-import java.util.Arrays;
-import java.util.List;
+import com.TiendaM_IQ2023.service.ClienteService;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.weaver.ast.Var;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
 @Controller
 public class IndexController {
 
     @Autowired
-    ClienteDao clienteDao;
-    
+    ClienteService clienteService;
+
     @GetMapping("/")
     public String inicio(Model model) {
-     //   log.info("Ahora desde MVC");
-      //  model.addAttribute("Mensaje", "Hola desde el controlador");
+        //   log.info("Ahora desde MVC");
+        //  model.addAttribute("Mensaje", "Hola desde el controlador");
 
-      //  Cliente cliente = new Cliente("Gerald", "Calderon Serrano", "geraldesteban06@gmail.com", "83342258");
-      //  model.addAttribute("objetoCliente", cliente);
-
-       // Cliente cliente2 = new Cliente("Laura", "Serrano Zuñiga", "lau1211@hotmail.com", "84300455");
-       // Cliente cliente3 = new Cliente("Emerson", "Barrantes Luna", "Emer1710@hotmail.com", "55778899");
-
-       var clientes = clienteDao.findAll();
+        //  Cliente cliente = new Cliente("Gerald", "Calderon Serrano", "geraldesteban06@gmail.com", "83342258");
+        //  model.addAttribute("objetoCliente", cliente);
+        // Cliente cliente2 = new Cliente("Laura", "Serrano Zuñiga", "lau1211@hotmail.com", "84300455");
+        // Cliente cliente3 = new Cliente("Emerson", "Barrantes Luna", "Emer1710@hotmail.com", "55778899");
+        var clientes = clienteService.getClientes();
         model.addAttribute("clientes", clientes);
         return "index";
     }
 
+    @GetMapping("/nuevoCliente")
+    public String nuevoCliente(Cliente cliente) {
+        return "modificarCliente";
+    }
+
+    @PostMapping("/guardarCliente")
+    public String guardarCliente(Cliente cliente) {
+        clienteService.save(cliente);
+        return "redirect:/";
+    }
+
+    @GetMapping("/modificarCliente/{idCliente}")
+    public String modificarCliente(Cliente cliente, Model model) {
+        cliente = clienteService.getCliente(cliente);
+        model.addAttribute("cliente", cliente);
+        return "modificarCliente";
+    }
+
+    @GetMapping("/eliminarCliente/{idCliente}")
+    public String eliminarCliente(Cliente cliente) {
+        clienteService.delete(cliente);
+        return "redirect:/";
+    }
 }
